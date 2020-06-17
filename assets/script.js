8506fbd0f6aa1626281c36224fcb8cbde59ca52a
kalebgarrison <kalebgarrison@gmail.com>
2020-06-17 09:30:15 -0400

    added dynamic 5 day forecast. Also added weather icons." " "

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,112 @@
 console.log("hello");
+var now = moment();
+var monthDay = moment().format("dddd, MMMM Do, YYYY");
 
 // var queryUrl =
 //   "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=atlanta&appid=4c4e0d346a190f1bd944059aca6f817d";
 
 $("#search-button").on("click", function (event) {
   event.preventDefault();
+  $("#five-day-forecast").empty()
+  
+  var apiKey = "4c4e0d346a190f1bd944059aca6f817d";
   var cityName = $("#search-bar").val();
 
   var queryUrl =
     "http://api.openweathermap.org/data/2.5/weather?q=" +
     cityName +
     "&appid=4c4e0d346a190f1bd944059aca6f817d";
-  console.log(cityName);
+
+ 
   $.ajax({
     url: queryUrl,
     method: "GET",
   }).then(function (response) {
     console.log(response);
-    console.log(response.weather[0].main)
+    var tempF = (response.main.temp - 273.15) * 1.8 + 32;
+    var lat = response.coord.lat;
+    var lon = response.coord.lon;
+    var headerIcon = response.weather[0].icon
+    var iconUrl = "http://openweathermap.org/img/w/" + headerIcon + ".png";
+    console.log(iconUrl)
+    $('#icon-header').attr('src', iconUrl)
+
+
+    var uvIndex = $("#city-header").text(response.name + " " + monthDay);
+    $("#temp").text("Temperature (F) " + tempF.toFixed(2));
+    $("#humidity").text("Humidity: " + response.main.humidity + "%");
+    $("#wind-speed").text("Wind Speed: " + response.wind.speed + "mph");
+    var geoUrl =
+      "http://api.openweathermap.org/data/2.5/uvi?appid=" +
+      apiKey +
+      "&lat=" +
+      lat +
+      "&lon=" +
+      lon;
+
+    $.ajax({
+      url: geoUrl,
+      method: "GET",
+    }).then(function (geo) {
+      console.log(geo);
+      var uvIndex = $("#uv-index").text("UV Index: " + geo.value);
+    });
+    var forecastUrl =
+      "http://api.openweathermap.org/data/2.5/forecast?q=" +
+      cityName +
+      "&appid=" +
+      apiKey;
+    $.ajax({
+      url: forecastUrl,
+      method: "GET",
+    }).then(function (forecast) {
+      var forecastIdArray = [1, 2, 3, 4, 5];
+      var fiveDayArray = [1, 2, 3, 4, 5];
+      var fiveId = $("five-day-" + forecastIdArray[i]);
+
+      for (var i = 5; i < forecast.list.length; i += 8) {
+        var fiveDayForecast = forecast.list[i];
+        var currentDate = moment(forecast.list[i].dt, "X").format("MM/DD/YYYY");
+        var tempF = (fiveDayForecast.main.temp_max - 273.15) * 1.8 + 32;
+        var forecastIcon = fiveDayForecast.weather[0].icon
+        var fiveUrl = "http://openweathermap.org/img/w/" + forecastIcon + ".png";
+        console.log(forecastIcon)
+
+
+        var fiveBox = $("<div></div>");
+        fiveBox.addClass("col-2 five-day-box");
+        var fiveP = $("<p></p>");
+        fiveP.addClass("fiveStyle");
+        var fivePDiv = $("<div></div>");
+        fiveBox.addClass("col-2 five-day-box");
+        var fiveIcon = $("<img>");
+        fiveIcon.addClass("fiveStyle");
+        fiveIcon.attr('src', fiveUrl)
+        var fiveTemp = $("<p></p>");
+        fiveTemp.addClass("fiveStyle");
+        var fiveHumid = $("<p></p>");
+        fiveHumid.addClass("fiveStyle");
+
+        $("#five-day-forecast").append(fiveBox);
+        $("#five-day-forecast").append(fivePDiv);
+        fiveBox.append(fiveP);
+        fiveBox.append(fiveIcon);
+        fiveBox.append(fiveTemp);
+        fiveBox.append(fiveHumid);
+
+        fiveP.text(currentDate);
+        fiveTemp.text("Temp: " + tempF.toFixed(2) + " F");
+        fiveIcon.text("placeholder");
+        fiveHumid.text('Humidity: ' + fiveDayForecast.main.humidity + '%');
+        console.log(fiveDayForecast.main.humidity);
+      }
+      console.log(forecast.list.length);
+
+      console.log(forecast);
+    });
+
+    console.log(response);
+    console.log(response.weather[0].main);
   });
 });
 // $.ajax({
